6a5194ef5847654a4c56988839638b36892f2c83
Ali Ahmed Hamad <es-ali.ahmed2027@alexu.edu.eg>
2025-10-13 15:21:36 +0300

    implement user model, database connection, and update environment configuration

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.route.js';
+import { connectDB } from "./libs/db.js";
 
 dotenv.config();
 
@@ -13,4 +14,6 @@ app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+
+  connectDB();
 });
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Student-AI-assistant
+# Ecommerce Website
+
