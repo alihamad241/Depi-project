f1b0f9fcbab3830c23998a4319e0ce90fc93f211
Ali Ahmed Hamad <es-ali.ahmed2027@alexu.edu.eg>
2025-10-13 23:41:58 +0300

    Improves routing with auth-based access and new pages
    Introduces authentication checks to route access, adding redirects for protected and guest-only pages.
    Adds admin, category, cart, and purchase flow pages to expand functionality.
    Displays a loading spinner during auth check for better UX.
    Integrates toaster notifications for user feedback.

---
+++ Frontend/src/stores/useProductStore.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const useProductStore = () => {
+  return (
+    <div>useProductStore</div>
+  )
+}
+
+export default useProductStore
---
+++ Frontend/src/stores/useUserStore.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const useUserStore = () => {
+  return (
+    <div>useUserStore</div>
+  )
+}
+
+export default useUserStore
