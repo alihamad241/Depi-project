b7f9b929ad46419b6e10ff02225a6b25a9079b1f
kithsope <malakhassanabdelhamid@gmail.com>
2025-10-16 15:37:57 +0300

    updated useProductStore to use in CategoryPage

--- Frontend/src/stores/useProductStore.js
+++ Frontend/src/stores/useProductStore.js
@@ -29,6 +29,16 @@ export const useProductStore=create((set)=>({
       toast.error(error.response.data.error||"Failed to fetch products");
     }
   },
+  fetchProductByCategory:async()=>{
+    set({loading:true});
+    try{
+      const response=await axios.get(`/products/category/${category}`);
+      set({products:response.data.products,loading:false});
+    }catch(error){
+      set({error:"Faled to fetch products",loading:false});
+      toast.error(error.response.data.error||"Failed to fetch products")
+    }
+  },
   deleteProduct: async(productId)=>{
     set({loading:true});
     try{
