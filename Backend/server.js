de273255d2870411e58cb88c156187d708d70b73
Karim Abogharbia <karim91_@hotmail.com>
2025-10-17 01:32:34 +0300

    added the tokens/auth backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';
-import { connectDB } from "./libs/db.js";
+import { connectDB } from './libs/db.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-app.use("/api/auth", authRoutes)
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 
-  connectDB();
+    connectDB();
 });
