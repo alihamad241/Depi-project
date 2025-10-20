3bbee0e58bb99fc69aa0282c08ce6e1e3716edc0
Karim Abogharbia <karim91_@hotmail.com>
2025-10-20 15:14:44 +0300

    completed add to cart, remove from cart, update item quantity in cart

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 
 import productRoutes from './routes/product.route.js';
 import authRoutes from './routes/auth.route.js';
+import cartRoutes from './routes/cart.route.js';
 import paymentRoutes from './routes/payment.route.js';
 import { connectDB } from './libs/db.js';
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
+app.use("/api/cart", cartRoutes);
 app.use('/api/payment', paymentRoutes);
 
 app.listen(PORT, () => {
